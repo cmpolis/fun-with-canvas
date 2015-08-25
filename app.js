78a7d5a2f64935918957ec7cba45a1e328e26021
Chris Polis <cmpolis@gmail.com>
2015-08-24 22:33:10 -0700

    slightly smarter render

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var width = 600,
     height = 400,
-    radius = 2.5,
+    radius = 1.5,
     pointCount = 10e3,
     renderCount = 10,
     canvas = document.getElementById('scatterplot'),
@@ -36,9 +36,23 @@ var renderA = function(context, points) {
   });
 };
 
+// Group by color (fewer fill changes)
+var groupRender = function(context, points) {
+  context.globalAlpha = 0.44;
+  _.forEach(_.groupBy(points, 'color'), function(points, color) {
+    context.fillStyle = color;
+    points.forEach(function(point, ndx) {
+      context.beginPath();
+      context.arc(point.x, point.y, radius, 0, 2 * Math.PI, false);
+      context.fill();
+    });
+  })
+};
+
 var start;
 for(var renderNdx = 0; renderNdx < renderCount; renderNdx++) {
   start = performance.now();
-  renderA(context, points);
+  //renderA(context, points);
+  groupRender(context, points);
   console.log((performance.now() - start) + ' ms.');
 }
