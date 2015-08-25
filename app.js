7a4b187f5d6747b72e6c35d46efe67a3fdbfb207
Chris Polis <cmpolis@gmail.com>
2015-08-24 22:50:16 -0700

    drag to select interactions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var width = 600,
     height = 400,
     radius = 1.5,
     pointCount = 10e3,
-    renderCount = 10,
+    renderCount = 1,
     canvas = document.getElementById('scatterplot'),
     context = canvas.getContext('2d'),
     palette = '#E9AEC8 #9ADF97 #DCCB6A #77DFD3 #A5C8E4'.split(' ');
@@ -22,8 +22,8 @@ var generatePoint = function() {
 };
 var points = _.range(pointCount).map(generatePoint);
 
-//
-//
+///////////////////////////
+// RENDERING
 
 // Simple render
 var renderA = function(context, points) {
@@ -31,7 +31,7 @@ var renderA = function(context, points) {
   points.forEach(function(point, ndx) {
     context.beginPath();
     context.arc(point.x, point.y, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = point.color;
+    context.fillStyle = point.isSelected ? 'black' : point.color;
     context.fill();
   });
 };
@@ -56,3 +56,58 @@ for(var renderNdx = 0; renderNdx < renderCount; renderNdx++) {
   groupRender(context, points);
   console.log((performance.now() - start) + ' ms.');
 }
+
+///////////////////////////
+// INTERACTION
+// http://www.html5canvastutorials.com/advanced/html5-canvas-mouse-coordinates/
+
+var isDragging = false, initialPos;
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+  //console.log(mousePos);
+  if(isDragging) {
+    var mousePos = getMousePos(canvas, evt),
+        minX = Math.min(mousePos.x, initialPos.x),
+        maxX = Math.max(mousePos.x, initialPos.x),
+        minY = Math.min(mousePos.y, initialPos.y),
+        maxY = Math.max(mousePos.y, initialPos.y);
+    points.forEach(function(point) {
+      point.isSelected = point.x >= minX && point.x <= maxX &&
+                         point.y >= minY && point.y <= maxY;
+    });
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    renderA(context, points);
+  }
+}, false);
+canvas.addEventListener('mousedown', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  initialPos = mousePos;
+  isDragging = true;
+}, false);
+canvas.addEventListener('mouseup', function(evt) {
+  var mousePos = getMousePos(canvas, evt),
+      minX = Math.min(mousePos.x, initialPos.x),
+      maxX = Math.max(mousePos.x, initialPos.x),
+      minY = Math.min(mousePos.y, initialPos.y),
+      maxY = Math.max(mousePos.y, initialPos.y);
+
+  isDragging = false;
+  // var pointsInBounds = points.filter(function(point) {
+  //   return point.x >= minX && point.x <= maxX &&
+  //          point.y >= minY && point.y <= maxY;
+  // });
+  points.forEach(function(point) {
+    point.isSelected = point.x >= minX && point.x <= maxX &&
+                       point.y >= minY && point.y <= maxY;
+  });
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  renderA(context, points);
+}, false);
